6a330dc855298ce563767449b3f574346eb8b9da
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-03 09:55:53 +0100

    feat: test to check content renders on click

--- test-area/src/App.test.js
+++ test-area/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import App from './App';
 import './__mocks__/intersectionObserverMock';
 
@@ -11,6 +11,32 @@ describe('App', () => {
     expect(screen.getByText('2019')).toBeInTheDocument();
     expect(screen.getByText('2018')).toBeInTheDocument();
     expect(screen.queryByText('2017')).not.toBeInTheDocument();
+  });
+
+  it('renders and derenders content on click', () => {
+    render(<App />);
+
+    expect(screen.queryByText('匆匆 朱自清 散文')).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(
+        '燕子去了，有再来的时候；杨柳枯了，有再青的时候；桃花谢 了，有再开的时候。但是，聪明的，你告诉我，我们的日子为什么一去不复返呢？--是有人 偷了他们罢：那是谁？又藏在何处呢？是他们自己逃走了罢：现在又到了哪里呢？',
+      ),
+    ).not.toBeInTheDocument();
+    fireEvent.click(screen.getByText('九月十六日，星期三'));
+    expect(screen.getByText('匆匆 朱自清 散文')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        '燕子去了，有再来的时候；杨柳枯了，有再青的时候；桃花谢了，有再开的时候。但是，聪明的，你告诉我，我们的日子为什么一去不复返呢？--是有人偷了他们罢：那是谁？又藏在何处呢？是他们自己逃走了罢：现在又到了哪里呢？',
+      ),
+    ).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('九月十六日，星期三'));
+    expect(screen.queryByText('匆匆 朱自清 散文')).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(
+        '燕子去了，有再来的时候；杨柳枯了，有再青的时候；桃花谢了，有再开的时候。但是，聪明的，你告诉我，我们的日子为什么一去不复返呢？--是有人偷了他们罢：那是谁？又藏在何处呢？是他们自己逃走了罢：现在又到了哪里呢？',
+      ),
+    ).not.toBeInTheDocument();
   });
 });
 
