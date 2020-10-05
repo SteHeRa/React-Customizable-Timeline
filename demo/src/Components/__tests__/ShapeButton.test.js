1f66699a75a2781735848cbafc689ac029a68a27
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-05 14:13:15 +0100

    feat: added first test for ShapeButton

---
+++ demo/src/Components/__tests__/ShapeButton.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ShapeButton from '../ShapeButton/ShapeButton';
+
+describe('ShapeButton', () => {
+  it('calls when clicked', () => {});
+
+  const setShapes = jest.fn();
+
+  render(
+    <ShapeButton
+      shapes={{
+        titleShape: 'circle',
+        dotShape: 'circle',
+        lineShape: 'solid',
+      }}
+      setShapes={setShapes}
+    />,
+  );
+
+  for (let i = 0; i < 12; i++) {
+    userEvent.click(screen.getAllByRole('radio')[i]);
+  }
+
+  expect(setShapes).toHaveBeenCalledTimes(12);
+});
